2f4e1450b23cc868c9cd5ce49ccfb059a62c240d
DESKTOP-EMC39NL\samho <sholzer20@my.whitworth.edu>
2017-05-13 13:30:17 -0700

    Setting up files

--- FInal_Project/FInal_Project_Ben_and_Sam/HospitalStaff.h
+++ FInal_Project/FInal_Project_Ben_and_Sam/HospitalStaff.h
@@ -1,4 +1,8 @@
 #ifndef HOSPITALSTAFF_H_
 #define HOSPITALSTAFF_H_
 
+class HospitalStaff {
+
+};
+
 #endif
--- FInal_Project/FInal_Project_Ben_and_Sam/Nurse.h
+++ FInal_Project/FInal_Project_Ben_and_Sam/Nurse.h
@@ -1,4 +1,8 @@
 #ifndef NURSE_H_
 #define NURSE_H_
+#include "HospitalStaff.h"
 
+class Nurse : HospitalStaff {
+
+};
 #endif
