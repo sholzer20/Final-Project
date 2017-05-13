7c41aa341a52bb407b2a3625d3dfab925ee89eff
DESKTOP-EMC39NL\samho <sholzer20@my.whitworth.edu>
2017-05-13 14:04:18 -0700

    Completed Record class (hopefully permanently)

--- FInal_Project/FInal_Project_Ben_and_Sam/Record.cpp
+++ FInal_Project/FInal_Project_Ben_and_Sam/Record.cpp
@@ -1,1 +1,33 @@
 #include "Record.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+Record::Record()
+{
+	visits = 0;
+}
+
+// Concatenates the patient's history from # of visits and the vector of severities
+void Record::display()
+{
+	cout << "------------------- Patient's History -------------------"<< endl;
+	cout << "Visits to the hospital: " << visits << endl;
+	cout << "Severity upon each visit: ";
+	// Grabs each severity value, adds comma for each until the last
+	for (unsigned int i = 0; i < severities.size(); i++)
+	{
+		if (i != (severities.size() - 1))
+			cout << severities[i] << ", ";
+		else
+			cout << severities[i] << endl;
+	}
+
+	cout << "----------------------------------------------------------" << endl;
+}
+
+void Record::updateFile(int severity)
+{
+	visits++;
+	severities.push_back(severity);
+}
--- FInal_Project/FInal_Project_Ben_and_Sam/Record.h
+++ FInal_Project/FInal_Project_Ben_and_Sam/Record.h
@@ -1,10 +1,18 @@
 #ifndef RECORD_H
 #define RECORD_H
+#include <vector>
+#include <string>
 
 class Record {
 
+private:
+	int visits;
+	std::vector<int> severities;
 
-
+public:
+	Record();
+	void display();
+	void updateFile(int severity);
 };
 
 
