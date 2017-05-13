c66f41ba8888f89c67ae6d4b23b56ac4b2f35f08
DESKTOP-EMC39NL\samho <sholzer20@my.whitworth.edu>
2017-05-13 14:22:28 -0700

    Completed the Person class

--- FInal_Project/FInal_Project_Ben_and_Sam/Person.cpp
+++ FInal_Project/FInal_Project_Ben_and_Sam/Person.cpp
@@ -1,1 +1,26 @@
 #include "Person.h"
+
+Person::Person(std::string name)
+{
+	this->name = name;
+}
+
+int Person::getSeverity()
+{
+	return severity;
+}
+
+void Person::setSeverity(int severity)
+{
+	this->severity = severity;
+}
+
+void Person::setSickness(bool sick)
+{
+	isSick = sick;
+}
+
+bool Person::getSickness()
+{
+	return isSick;
+}
