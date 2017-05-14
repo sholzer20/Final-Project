dc74360e22294cf67b5d99e72619c55658a3ec56
DESKTOP-EMC39NL\samho <sholzer20@my.whitworth.edu>
2017-05-13 21:48:35 -0700

    Started defining Hospital class

--- FInal_Project/FInal_Project_Ben_and_Sam/Hospital.cpp
+++ FInal_Project/FInal_Project_Ben_and_Sam/Hospital.cpp
@@ -1,1 +1,13 @@
 #include "Hospital.h"
+
+Hospital::Hospital()
+{
+}
+
+void Hospital::assignSeverity(Person * patient)
+{
+}
+
+void Hospital::updateRecord(Person * patient)
+{
+}
--- FInal_Project/FInal_Project_Ben_and_Sam/Hospital.h
+++ FInal_Project/FInal_Project_Ben_and_Sam/Hospital.h
@@ -1,11 +1,28 @@
 #ifndef HOSPITAL_H
 #define HOSPITAL_H
+#include "HospitalStaff.h"
+#include "Person.h"
+#include "Record.h"
+#include <vector>
+#include <queue>
+#include <string>
+#include <map>
+using std::vector;
+using std::string;
 
 class Hospital {
 
+private:
+	int totalTime;
+	int totalServed;
+	vector<HospitalStaff*> hospitalStaff;
+	vector<string> patientRoster;
+	std::map<string, Record*> patientRecords;
 
-
+public:
+	Hospital();
+	void assignSeverity(Person* patient);
+	void updateRecord(Person* patient);
 };
-
 
 #endif
